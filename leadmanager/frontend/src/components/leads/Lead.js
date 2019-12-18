496d3ef23bf125ef67f827616d71d58b889f8114
Vinícius Miranda <viniciusc.miranda@outlook.com>
2019-12-18 18:03:02 -0200

    setting react environment

---
+++ leadmanager/frontend/src/components/leads/Lead.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Lead extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Leads List</h1>
+      </div>
+    )
+  }
+}
